e2a0b7ea30126de35dc703d6a70d665a4b40faae
Nistoj Bhattarai <135168799+knistozb@users.noreply.github.com>
2023-06-29 21:37:54 -0500

    2nd commit test2

---
+++ src/nb_launchBrowser_pkg/GitTest2.java
@@ -0,0 +1,20 @@
+
+/// Selenium Class 8.c:- June 29, 2023, Thursday with new Instructor Shihabul Khandakar (Shibab) 
+// Based on previous Dec 2022 Batch, April 16, 2023:
+
+
+package nb_launchBrowser_pkg;
+
+public class GitTest2 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Git Testt");
+		System.out.println("Git Testt 1");
+		System.out.println("Git Testt 2");
+		System.out.println("Git Testt 3");
+		System.out.println("Git Testt 4");
+
+	}
+
+}
